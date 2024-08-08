64c2b9456d426132881659be61337584ba638de0
jiyanishar <jiya.17081@sakec.ac.in>
2024-08-08 11:26:56 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
- public class Hello{
+ public class hello{
 
 	public static void main(String[] args){
 		
-		for(int i=1;i<=10;++){
+		for(int i=1;i<=10;i++){
 
 			System.out.println("Hello World"+i);
 
